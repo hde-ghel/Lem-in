d8525972b383620c20c58c03ded6f4976a5e07b6
user42 <hde.ghel@gmail.com>
2020-07-23 12:57:15 +0200

    belman ford first implementation

--- include/lem_in.h
+++ include/lem_in.h
@@ -40,6 +40,7 @@ struct	s_room
 	int						type;
 	unsigned long	key;
 	int						weight;
+	t_room				*path_next;
 	t_link				*link_list; //room_links
 	t_room				*next; //liste cree en cas de hascode les meme
 };
@@ -51,6 +52,7 @@ struct	s_link
 	int				weight;
 	int				visited;
 	int				inversed;
+	int				active;
 	t_link		*reverse;
 	t_link		*room_link_next;
 	t_link		*list_next;
@@ -113,5 +115,10 @@ t_room		*get_room_by_hash(t_lemin *env, unsigned long key, char *room);
 */
 void print_room_map(t_lemin *env);
 void print_link_list(t_lemin *env);
+
+/*
+ * find_paths.c
+*/
+void find_paths(t_lemin *env);
 
 #endif
--- src/find_paths.c
+++ src/find_paths.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
+/*   parse_rooms.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,20 +12,44 @@
 
 #include "../include/lem_in.h"
 
-//Pas sur de recup les comment
-void		get_comment(char *str)
+
+int		bellwhile_ford(t_link *link, t_lemin *env)
 {
-	ft_printf("%s\n", str);
+	int changed;
+
+	changed = 0;
+	while (link)
+	{
+			if (link->room_a->weight != MAX_WEIGHT
+				&& link->room_a->weight + link->weight < link->room_b->weight
+				&& link->room_b != env->start && link->room_a != env->end)
+			{
+				link->room_b->weight = link->weight + link->room_a->weight;
+				link->room_b->path_next = link->room_a;
+				changed = 1;
+		}
+		link = link->list_next;
+	}
+	return (changed);
 }
 
-void	parse_input(t_lemin *env)
+void		bellman_ford(t_lemin *env)
 {
-	if (isatty(env->fd))
-		error_msg(env, "No map file specified");
-	parse_ants(env);
-	printf("nb ants = %d\n\n", env->nb_ants);
-	parse_rooms(env);
-	parse_links(env);
-	//print_room_map(env);
-	//print_link_list(env);
+	t_link	*link;
+	int			countrooms;
+
+	countrooms = env->nb_rooms;
+	link = env->links_map;
+	while (--countrooms > 0 && link)
+	{
+		link = env->links_map;
+		if (bellwhile_ford(link, env) == 0)
+			break ;
+	}
+}
+
+
+void    find_paths(t_lemin *env)
+{
+  bellman_ford(env);
 }
