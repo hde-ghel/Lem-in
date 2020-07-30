2552b6178ce7465473005ecb7989a8df43f739a6
Alborz Babaie Mouziraji <ababaie-@student.s19.be>
2020-07-30 17:02:32 +0200

    Replaced printf with ft_printf in l26 of parsing.c

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 14:54:59 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/03/08 18:35:52 by hde-ghel         ###   ########.fr       */
+/*   Updated: 2020/07/30 17:01:52 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	parse_input(t_lemin *env)
 	if (isatty(env->fd))
 		error_msg(env, "No map file specified");
 	parse_ants(env);
-	printf("Number of ants = %d\n\n", env->nb_ants);
+	ft_printf("Number of ants = %d\n\n", env->nb_ants);
 	parse_rooms(env);
 	parse_links(env);
 	//print_room_map(env);
