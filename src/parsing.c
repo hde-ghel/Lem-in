bd7d65df229a63fc20832d9cc4d7d4e6f563046f
Alborz Babaie Mouziraji <ababaie-@student.s19.be>
2020-07-30 17:04:54 +0200

    Added a '\n' to string in error_msg.

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 14:54:59 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/07/30 17:01:52 by ababaie-         ###   ########.fr       */
+/*   Updated: 2020/07/30 17:04:17 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		get_comment(char *str)
 void	parse_input(t_lemin *env)
 {
 	if (isatty(env->fd))
-		error_msg(env, "No map file specified");
+		error_msg(env, "No map file specified\n");
 	parse_ants(env);
 	ft_printf("Number of ants = %d\n\n", env->nb_ants);
 	parse_rooms(env);
