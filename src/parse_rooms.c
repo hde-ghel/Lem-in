908c1461bc96970625118a6419f9e43e2acc697a
Alborz Babaie Mouziraji <ababaie-@student.s19.be>
2020-08-21 17:51:50 +0200

    Corrected some of the norm errors in parse_rooms.c

--- src/parse_rooms.c
+++ src/parse_rooms.c
@@ -6,37 +6,37 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 14:54:59 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/08/21 17:35:43 by ababaie-         ###   ########.fr       */
+/*   Updated: 2020/08/21 17:49:45 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem_in.h"
 
-# define ERROR_MEM "ERROR: memory allocation failed"
-# define ERROR_START "ERROR: more than one room ##start"
-# define ERROR_END "ERROR: more than one room ##end"
-# define ERROR_READ "ERROR: reading error or no pipes given"
-# define ERROR_CMD "ERROR: command unknown"
+#define ERROR_MEM "ERROR: memory allocation failed"
+#define ERROR_START "ERROR: more than one room ##start"
+#define ERROR_END "ERROR: more than one room ##end"
+#define ERROR_READ "ERROR: reading error or no pipes given"
+#define ERROR_CMD "ERROR: command unknown"
 
-t_xy save_room_coord(char *line)
+t_xy		save_room_coord(char *line)
 {
-	t_xy 		coord;
-	int			i;
+	t_xy	coord;
+	int		i;
 
 	i = 0;
 	while (line[i] != ' ')
 		i++;
 	coord.x = ft_atoi(&line[i]);
 	i++;
-	while(line[i] != ' ')
-	i++;
-	coord.y = ft_atoi(&line[i]);;
+	while (line[i] != ' ')
+		i++;
+	coord.y = ft_atoi(&line[i]);
 	return (coord);
 }
 
 void		new_room(t_lemin *env, char *line) //peut etre rajouter une check savoir si la room existe deja
 {
-	t_room		*new;
+	t_room	*new;
 
 	if (!(new = malloc(sizeof(t_room))))
 	{
@@ -45,22 +45,22 @@ void		new_room(t_lemin *env, char *line) //peut etre rajouter une check savoir s
 	}
 	ft_bzero(new, sizeof(t_room));
 	if (!(new->name = ft_strsub(line, 0, strchr(line, ' ') - line)))
-	 	{
-			ft_strdel(&line);
-			free(new);
-			error_msg(env, "ERROR: memory allocation failed", 1);
-		}
+	{
+		ft_strdel(&line);
+		free(new);
+		error_msg(env, "ERROR: memory allocation failed", 1);
+	}
 	new->coord = save_room_coord(line);
 	new->key = hash_key(new->name);
 	env->nb_rooms++;
 	new->weight = MAX_WEIGHT;
 	if (env->start_room == 1)
-		{
-			env->start_room++;
-			env->start = new;
-			new->weight = 0;
-			new->type = 1;
-		}
+	{
+		env->start_room++;
+		env->start = new;
+		new->weight = 0;
+		new->type = 1;
+	}
 	else if (env->end_room == 1)
 	{
 		env->end_room++;
@@ -70,33 +70,33 @@ void		new_room(t_lemin *env, char *line) //peut etre rajouter une check savoir s
 	if (env->map[new->key] == NULL)
 		env->map[new->key] = new;
 	else
-		{
-			new->next = env->map[new->key];
-			env->map[new->key] = new;
-		}
+	{
+		new->next = env->map[new->key];
+		env->map[new->key] = new;
+	}
 }
 
-static void		get_command(t_lemin *env, char *line)
+static void	get_command(t_lemin *env, char *line)
 {
 	if (ft_strequ(line, "##start"))
+	{
+		if (env->start_room != 0)
 		{
-			if (env->start_room != 0)
-			{
-				ft_strdel(&line);
-				error_msg(env, "ERROR more than one room ##start", 1);
-      }
-			else
-				env->start_room = 1;
+			ft_strdel(&line);
+			error_msg(env, "ERROR more than one room ##start", 1);
 		}
+		else
+			env->start_room = 1;
+	}
 	else if (ft_strequ(line, "##end"))
 	{
-			if (env->end_room != 0)
-			{
-				ft_strdel(&line);
-				error_msg(env, "ERROR more than 1 room ##end", 1);
-			}
-			else
-				env->end_room = 1;
+		if (env->end_room != 0)
+		{
+			ft_strdel(&line);
+			error_msg(env, "ERROR more than 1 room ##end", 1);
+		}
+		else
+			env->end_room = 1;
 	}
 	//else error command unknown or skip
 }
@@ -115,14 +115,14 @@ int		count_space(char *line)
 	return (space);
 }
 
-static int		isroom(char *line)
+static int	isroom(char *line)
 {
 	int		i;
 
 	i = 0;
 	if (line[0] == '#' || line[0] == 'L' || count_space(line) != 2)
 		return (0);
-	while(line[i] != ' ')
+	while (line[i] != ' ')
 	{
 		if (line[i] == '-')
 			return (0);
@@ -137,7 +137,7 @@ static int		isroom(char *line)
 	i++;
 	if (line[i] == '-' || line[i] == '+')
 		i++;
-	while(line[i])
+	while (line[i])
 		if (ft_isdigit(line[i++]) == 0)
 			return (0);
 	return (1);
@@ -157,12 +157,12 @@ void		parse_rooms(t_lemin *env)
 		else if (isroom(env->line))
 			new_room(env, env->line);
 		else
-			break;
+			break ;
 		ft_strdel(&env->line);
 	}
 	if (ret == -1 || ret == 0)
 	{
 		ft_strdel(&env->line);
 		error_msg(env, "ERROR: reading error or no pipes given", 1);
-  }
+	}
 }
