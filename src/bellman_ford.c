6832e4db1f5ab1c2c1134530e4798304953f5e48
Alborz Babaie Mouziraji <ababaie-@student.s19.be>
2020-08-21 17:54:20 +0200

    Corrected norm errors in bellman_ford.c

--- src/bellman_ford.c
+++ src/bellman_ford.c
@@ -6,7 +6,7 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 14:54:59 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/03/08 18:35:52 by hde-ghel         ###   ########.fr       */
+/*   Updated: 2020/08/21 17:53:56 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int		bellman_ford_bis(t_link *link, t_lemin *env)
 {
-	int changed;
+	int	changed;
 
 	changed = 0;
 	while (link)
 	{
-			if (link->room_a->weight != MAX_WEIGHT
-				&& link->room_a->weight + link->weight < link->room_b->weight
-				&& link->room_b != env->start && link->room_a != env->end)
-			{
-				link->room_b->weight = link->weight + link->room_a->weight;
-				link->room_b->path_next = link->room_a;
-				changed = 1;
+		if (link->room_a->weight != MAX_WEIGHT
+			&& link->room_a->weight + link->weight < link->room_b->weight
+			&& link->room_b != env->start && link->room_a != env->end)
+		{
+			link->room_b->weight = link->weight + link->room_a->weight;
+			link->room_b->path_next = link->room_a;
+			changed = 1;
 		}
 		link = link->list_next;
 	}
@@ -34,8 +34,8 @@ int		bellman_ford_bis(t_link *link, t_lemin *env)
 
 void		bellman_ford(t_lemin *env)
 {
+	int		countrooms;
 	t_link	*link;
-	int			countrooms;
 
 	countrooms = env->nb_rooms;
 	link = env->links_map;
